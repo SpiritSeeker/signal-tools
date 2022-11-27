d86db9d59b1a703567d556c6549de5d3d08d3893
Sai Sanjeet <sanjeet029@gmail.com>
2022-11-27 10:04:55 +0530

    feat: add sqaure wave to `signal_generator`

--- signal_generator.py
+++ signal_generator.py
@@ -1,6 +1,6 @@
 """
 Generation of various common signals.
-Each function generates and returns signals as a numpy array
+Each function generates and returns signals as a numpy array.
 
 ...
 
@@ -8,13 +8,19 @@ Functions
 ---------
 sine_wave(
         n_points: int, sample_rate: float,
-        fin: float, vpp: float = 2,
-        offset: float = 0, phase: float = 0
+        fin: float, **kwargs
     ) -> np.ndarray
     Generate a sine wave
+
+square_wave(
+        n_points: int, sample_rate: float,
+        fin: float, **kwargs
+    ) -> np.ndarray
+    Generate a sqaure wave
 """
 
 import numpy as np
+
 
 def sine_wave(
         n_points: int, sample_rate: float,
@@ -37,7 +43,7 @@ def sine_wave(
     **kwargs
         vpp: float
             Peak-to-peak voltage of the signal
-            Deafult value is `2`
+            Deafult value is `2`.
 
         offset: float
             Voltage offset of the signal (in V)
@@ -54,3 +60,48 @@ def sine_wave(
 
     return offset + (vpp / 2) * np.sin(2 * np.pi *
         ((fin/sample_rate * np.arange(n_points)) + phase/360))
+
+def square_wave(
+        n_points: int, sample_rate: float,
+        fin: float, **kwargs
+    ) -> np.ndarray:
+    """
+    Generate a square wave.
+
+    Parameters
+    ----------
+    n_points : int
+        Number of points in square wave to generate
+
+    sample_rate : float
+        Sample rate of the generated signal (in Hz)
+
+    fin : float
+        Input frequency of the generated signal (in Hz)
+
+    **kwargs
+        vpp: float
+            Peak-to-peak voltage of the signal
+            Deafult value is `1`.
+
+        offset: float
+            Voltage offset of the signal (in V)
+            Default value is `0.5`.
+
+        phase: float
+            Phase of the signal (in degrees)
+            Default value is `0`.
+    """
+
+    vpp = kwargs.get("vpp", 1)
+    offset = kwargs.get("offset", 0.5)
+    phase = kwargs.get("phase", 0)
+
+    # Generate sine wave to threshold and generate a square
+    sine = np.sin(
+        2 * np.pi * ((fin/sample_rate * np.arange(n_points)) + phase/360))
+    square = np.zeros(sine.shape)
+    square[sine < 0] = -vpp/2 + offset
+    square[sine >= 0] = vpp/2 + offset
+
+    return square
